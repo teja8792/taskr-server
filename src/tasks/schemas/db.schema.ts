f88d3834ae903c71d284cc03464e2ed0ea67570c
Bhanu Teja K <teja8792@gmail.com>
2021-08-03 12:27:00 +0530

    task schema update

--- src/tasks/schemas/db.schema.ts
+++ src/tasks/schemas/db.schema.ts
@@ -1,5 +1,11 @@
 import Joi from "joi";
 
 export default Joi.object({
-  status: Joi.string().default("unassigned")
+  status: Joi.string().default("unassigned"),
+  pickup: Joi.object({
+    datetime: Joi.string().default(Date.now())
+  }),
+  drop: Joi.any(),
+  has_pickup: Joi.bool().default(false),
+  has_drop: Joi.bool().default(false)
 });
